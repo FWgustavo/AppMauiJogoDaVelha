23c02cfc19bc478065af8d68d543280cd8b4f62e
Usuario <Usuario@CPD01-M06>
2024-03-08 12:21:34 -0300

    a

--- AppMauiJogoDaVelha/MainPage.xaml.cs
+++ AppMauiJogoDaVelha/MainPage.xaml.cs
@@ -48,6 +48,13 @@ namespace AppMauiJogoDaVelha
             {
                 DisplayAlert("Parabéns!", "O X ganhou", "Fechar");
             }
+            //Verificando  se a O ganhou na 2 linha
+            else if (btn20.Text == "O" &&
+                     btn21.Text == "O" &&
+                     btn22.Text == "O")
+            {
+                DisplayAlert("Parabéns!", "A O ganhou", "Fechar");
+            }
 
             //Verificando  se o X ganhou na 3 linha
             if (btn30.Text == "X" &&
@@ -55,6 +62,13 @@ namespace AppMauiJogoDaVelha
                 btn32.Text == "X")
             {
                 DisplayAlert("Parabéns!", "O X ganhou", "Fechar");
+            }
+            //Verificando  se a O ganhou na 3 linha
+            else if (btn30.Text == "O" &&
+                     btn31.Text == "O" &&
+                     btn32.Text == "O")
+            {
+                DisplayAlert("Parabéns!", "A O ganhou", "Fechar");
             }
 
             //Verificando  se o X ganhou na D linha
@@ -64,6 +78,13 @@ namespace AppMauiJogoDaVelha
             {
                 DisplayAlert("Parabéns!", "O X ganhou", "Fechar");
             }
+            //Verificando  se a O ganhou na D linha
+            else if (btn10.Text == "O" &&
+                     btn21.Text == "O" &&
+                     btn32.Text == "O")
+            {
+                DisplayAlert("Parabéns!", "A O ganhou", "Fechar");
+            }
 
             //Verificando  se o X ganhou na D2 linha
             if (btn12.Text == "X" &&
@@ -71,6 +92,13 @@ namespace AppMauiJogoDaVelha
                 btn30.Text == "X")
             {
                 DisplayAlert("Parabéns!", "O X ganhou", "Fechar");
+            }
+            //Verificando  se a O ganhou na D2 linha
+            else if (btn12.Text == "O" &&
+                     btn21.Text == "O" &&
+                     btn30.Text == "O")
+            {
+                DisplayAlert("Parabéns!", "A O ganhou", "Fechar");
             }
 
             //Verificando  se o X ganhou na 1 coluna
@@ -80,6 +108,13 @@ namespace AppMauiJogoDaVelha
             {
                 DisplayAlert("Parabéns!", "O X ganhou", "Fechar");
             }
+            //Verificando  se a O ganhou na 1 Coluna
+            else if (btn10.Text == "O" &&
+                     btn20.Text == "O" &&
+                     btn30.Text == "O")
+            {
+                DisplayAlert("Parabéns!", "A O ganhou", "Fechar");
+            }
 
             //Verificando  se o X ganhou na 2 coluna
             if (btn11.Text == "X" &&
@@ -88,6 +123,13 @@ namespace AppMauiJogoDaVelha
             {
                 DisplayAlert("Parabéns!", "O X ganhou", "Fechar");
             }
+            //Verificando  se a O ganhou na 2 Coluna
+            else if (btn11.Text == "O" &&
+                     btn21.Text == "O" &&
+                     btn31.Text == "O")
+            {
+                DisplayAlert("Parabéns!", "A O ganhou", "Fechar");
+            }
 
             //Verificando  se o X ganhou na 3 Coluna
             if (btn12.Text == "X" &&
@@ -95,6 +137,26 @@ namespace AppMauiJogoDaVelha
                 btn32.Text == "X")
             {
                 DisplayAlert("Parabéns!", "O X ganhou", "Fechar");
+            }
+            //Verificando  se a O ganhou na 3 Coluna
+            else if (btn12.Text == "O" &&
+                     btn22.Text == "O" &&
+                     btn32.Text == "O")
+            {
+                DisplayAlert("Parabéns!", "A O ganhou", "Fechar");
+            }
+            //Verificando se deu Velha
+            else if (btn10.Text == "X" ^ btn10.Text == "O" &&
+                     btn11.Text == "X" ^ btn11.Text == "O" &&
+                     btn12.Text == "X" ^ btn12.Text == "O" &&
+                     btn20.Text == "X" ^ btn20.Text == "O" &&
+                     btn21.Text == "X" ^ btn21.Text == "O" &&
+                     btn22.Text == "X" ^ btn22.Text == "O" &&
+                     btn30.Text == "X" ^ btn30.Text == "O" &&
+                     btn31.Text == "X" ^ btn31.Text == "O" &&
+                     btn32.Text == "X" ^ btn32.Text == "O" &&)
+            {
+                DisplayAlert("Vixi!", "Deu Velha!", "Fechar");
             }
 
         }// Fecha Button_Clicked
